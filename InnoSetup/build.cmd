7fa0062da01287e6c3134996c62013b1731f4444
fschnitz <info@example.com>
2014-09-04 15:58:44 +0200

    sign installer

--- InnoSetup/build.cmd
+++ InnoSetup/build.cmd
@@ -4,3 +4,5 @@ del /Q Xsemmel\bin\Release\*
 msbuild Xsemmel.sln /p:Configuration=Release
 cd InnoSetup
 "C:\Program Files (x86)\Inno Setup 5\ISCC.exe" setup.iss
+
+signtool sign /f xsemmel.pfx /p xxx /t http://timestamp.verisign.com/scripts/timestamp.dll output\*.exe
---
+++ InnoSetup/makecert.cmd
@@ -0,0 +1,3 @@
+makecert -r -pe -n "CN=Xsemmel development" -sky exchange -ss my
+
+rem -> go to certmgr.msc, export as pfx
